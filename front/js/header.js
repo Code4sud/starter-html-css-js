20828413d032e21af93c7f744df4230399056449
fatima-elmouhine <fatima.elmouhine@laplateforme.io>
2024-11-05 21:07:47 +0100

    add header and change host ingress

--- front/js/header.js
+++ front/js/header.js
@@ -1,10 +1,10 @@
 const isEventFinished = true; // Définis cela selon ta condition
 const menuItems = [
-    { id: 1, label: 'Accueil', href: 'https://ia4sud.fr' },
-    { id: 2, label: 'Les vainqueurs du Hackathon', href: 'https://ia4sud.fr/#winners' },
-    { id: isEventFinished ? 3 : 2, label: 'Les sujets abordés', href: 'https://ia4sud.fr/#subjects' },
-    { id: isEventFinished ? 4 : 3, label: 'Réalisations', href: 'https://ia4sud.fr/#teams' },
-    { id: isEventFinished ? 5 : 4, label: 'À propos', href: 'https://ia4sud.fr/#about' },
+    { id: "01", label: 'Accueil', href: 'https://alert4sud.fr' },
+    { id: "02", label: 'Les vainqueurs du Hackathon', href: 'https://alert4sud.fr/#winners' },
+    { id: isEventFinished ? "03" : "02", label: 'Les sujets abordés', href: 'https://alert4sud.fr/#subjects' },
+    { id: isEventFinished ? "04" : "03", label: 'Réalisations', href: 'https://alert4sud.fr/#teams' },
+    { id: isEventFinished ? "05" : "04", label: 'À propos', href: 'https://alert4sud.fr/#about' },
 ];
 
 const menuItemsWithoutWinners = menuItems.filter(item => item.label !== 'Les vainqueurs du Hackathon');
@@ -30,40 +30,11 @@ const populateMenu = () => {
     });
 };
 
-// Scroll effect
-const handleScroll = () => {
-    const nav = document.querySelector('nav');
-    if (window.scrollY === 0) {
-        nav.style.background = 'transparent';
-        nav.style.boxShadow = 'none';
-    } else {
-        const scrollPercentage = Math.min(window.scrollY / (document.documentElement.scrollHeight - window.innerHeight), 1);
-        const gradientColors = [
-            [188, 104, 142],
-            [109, 67, 142],
-            [214, 100, 93]
-        ];
-        
-        const interpolatedColor = gradientColors.reduce((acc, color, index) => {
-            if (index === gradientColors.length - 1) return acc;
-            const nextColor = gradientColors[index + 1];
-            const segmentPercentage = Math.min(Math.max((scrollPercentage - index / (gradientColors.length - 1)) * (gradientColors.length - 1), 0), 1);
-            const r = Math.round(color[0] + (nextColor[0] - color[0]) * segmentPercentage);
-            const g = Math.round(color[1] + (nextColor[1] - color[1]) * segmentPercentage);
-            const b = Math.round(color[2] + (nextColor[2] - color[2]) * segmentPercentage);
-            return `rgb(${r}, ${g}, ${b})`;
-        }, '');
-        
-        nav.style.background = interpolatedColor;
-        nav.style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, 0.5)';
-    }
-    nav.style.transition = 'background 0.3s ease-out';
-};
+
 
 // Écouteurs d'événements
 menuToggle.addEventListener('click', toggleDrawer);
 closeDrawer.addEventListener('click', toggleDrawer);
-window.addEventListener('scroll', handleScroll);
 
 // Initialiser le menu
 populateMenu();
